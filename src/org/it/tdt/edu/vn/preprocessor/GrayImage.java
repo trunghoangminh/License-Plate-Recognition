bf88484d65967f86b2fb6067b890c1acd8dd7063
Hoang Minh Trung <hoangminhtrung1995@gmail.com>
2017-03-16 20:25:18 +0700

    Add GrayImage and OriginalImage

---
+++ src/org/it/tdt/edu/vn/preprocessor/GrayImage.java
@@ -0,0 +1,10 @@
+package org.it.tdt.edu.vn.preprocessor;
+
+/**
+ * 
+ * @author hmtrung This class use to process image in gray image as:gray,
+ *         binary, threshold.
+ */
+public class GrayImage {
+
+}
---
+++ src/org/it/tdt/edu/vn/preprocessor/OgrinalImage.java
@@ -0,0 +1,10 @@
+package org.it.tdt.edu.vn.preprocessor;
+/**
+ * 
+ * @author hmtrung
+ * This class use get Image from file or camera.
+ *
+ */
+public class OgrinalImage {
+
+}
